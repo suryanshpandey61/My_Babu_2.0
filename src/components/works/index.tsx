4277fde90ba6cbcadc0a6342561ae7d542c187cb
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-04-01 16:30:11 +0530

    Work Secton Responsive completed

--- src/components/works/index.tsx
+++ src/components/works/index.tsx
@@ -16,48 +16,48 @@ function Work() {
             <h1 className='md:text-[36px] text-[24px] flex md:justify-start justify-center font-medium'>How Mybabu works?</h1>
         </div>
         {/* grid-4 div  */}
-        <div className='lg:grid-cols-4 w-full border border-black  gap-y-10 md:grid-cols-2 grid-cols-1 grid mt-[2%] gap-x-[1%]'>
+        <div className='lg:grid-cols-4 w-full   gap-y-10 md:grid-cols-2 grid-cols-1 grid mt-[2%] gap-x-[1%]'>
             {/* Submit your request card  */}
-            <div className='border '>
+            <div className=' '>
                 <Image
                 src={SubmitImage}
                 alt='submit-your-request-image'
                 className='lg:w-full md:w-[95%] mx-auto'
                 />
                 <h1 className='flex items-center mt-[4%] md:ml-0 ml-11 gap-x-3 text-[18px] font-medium'> <Image src={OneImg} alt='1' /> Submit your Request:</h1>
-              <div className=''>
-              <p className='text-[14px] md:w-full w-[60%] border md:ml-0 ml-[25%] md:mt-0 mt-3 font-normal lg:pl-[16%] md:pt-2'>Fill out simple online form describing your needs.Our Intutive Interface ensures  that you provide all the neccessary details right away.</p>
-              </div>
+              
+              <p className='text-[14px] md:w-full w-[60%]  md:ml-0 ml-[25%] md:mt-0 mt-3 font-normal lg:pl-[16%] md:pt-2'>Fill out simple online form describing your needs.Our Intutive Interface ensures  that you provide all the neccessary details right away.</p>
+             
               </div>
             {/* Expert Matching card  */}
-            <div className='border border-black'>
+            <div className=''>
                 <Image
                 src={ExpertImage}
                 alt='submit-your-request-image'
-                className='lg:w-full md:w-[95%]'
+                className='lg:w-full md:w-[95%] mx-auto'
                 />
-                <h1 className='flex items-center mt-[4%] gap-x-3 text-[18px] font-medium'> <Image src={TwoImg} alt='2' /> Expert Matching:</h1>
-                <p className='text-[14px] lg:pl-[16%] md:pt-2'>Our system automatically connects you with a vetted Kagzi Sahayak who specializes in your required department.</p>
+                <h1 className='flex items-center mt-[4%] ml-11 md:ml-0 gap-x-3 text-[18px] font-medium'> <Image src={TwoImg} alt='2' /> Expert Matching:</h1>
+                <p className='text-[14px] md:w-full md:ml-0 w-[60%] lg:pl-[16%] md:pt-2 ml-[25%]'>Our system automatically connects you with a vetted Kagzi Sahayak who specializes in your required department.</p>
             </div>
             {/* Recive Assistant card  */}
-            <div className='border border-black'>
+            <div className=''>
                 <Image
                 src={AssitantImage}
                 alt='submit-your-request-image'
-                className='lg:w-full md:w-[95%]'
+                className='lg:w-full md:w-[95%] mx-auto'
                 />
-                <h1 className='flex items-center mt-[4%] gap-x-3 text-[18px] font-medium'> <Image src={ThreeImg} alt='3' />Recieve Assistant:</h1>
-                <p className='text-[14px] lg:pl-[16%] md:pt-2'>Work with the assigned expert who will guide you through every step of your paper-work process-from document verification to submission. </p>
+                <h1 className='flex ml-11 md:ml-0  items-center mt-[4%] gap-x-3 text-[18px] font-medium'> <Image src={ThreeImg} alt='3' />Recieve Assistant:</h1>
+                <p className='text-[14px]  md:w-full w-[60%] ml-[25%] md:ml-0 lg:pl-[16%] md:pt-2'>Work with the assigned expert who will guide you through every step of your paper-work process-from document verification to submission. </p>
             </div>
             {/* Track Progress Card  */}
-            <div className='border border-black'>
+            <div className=''>
                 <Image
                 src={ProgressImage}
                 alt='submit-your-request-image'
-                className='lg:w-full md:w-[95%]'
+                className='lg:w-full md:w-[95%] mx-auto'
                 />
-                <h1 className='flex items-center mt-[4%] gap-x-3 text-[18px] font-medium'> <Image src={FourImg} alt='4' />Track Your Progress:</h1>
-                <p className='text-[14px] lg:pl-[16%] md:pt-2'>Stay updated with the real time tracking on your application status untill completion.</p>
+                <h1 className='flex items-center mt-[4%] gap-x-3 ml-11 md:ml-0  text-[18px] font-medium'> <Image src={FourImg} alt='4' />Track Your Progress:</h1>
+                <p className='text-[14px] md:w-full w-[60%] lg:pl-[16%] md:pt-2 ml-[25%] md:ml-0'>Stay updated with the real time tracking on your application status untill completion.</p>
             </div>
         </div>
 
