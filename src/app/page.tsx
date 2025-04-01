b0b2aff77ea75688c509f0f7c1496ef4a10225f8
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-04-01 14:55:31 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from "@/components/herosection";
+import Navbar from "@/components/navbar";
+
+
+export default function Home() {
+  return (
+    <div className="">
+     <Navbar/>
+     <HeroSection/>
+    </div>
+  );
+}
---
+++ src/components/herosection/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Image from 'next/image'
+import heroSectionUser from '../../assests/heroSectionUser.png'
+import rentAgreementImg from '../../assests/rentAgreementImg.png'
+import jobsAvailablebtn from '../../assests/jobsAvailablebtn.png'
+import dottedLineImg from '../../assests/dottedLineImg.png'
+import online from '../../assests/onlineApplicationImg.png'
+import call from '../../assests/call.png'
+import graph from '../../assests/graph.png'
+function HeroSection() {
+  return (
+    <div 
+    id='hero-section'
+    className='md:w-full w-[80%]  mx-auto'>
+        <div className='md:w-[70%] w-full ml-0 border border-black   justify-center  mt-[5%] mx-auto  gap-[20px] text-center '>
+              <h1 className='text-[36px] font-semibold justify-center text-center text-[#004016]'>
+                Your Personal Assistant for Hassle-Free 
+                Government Paperwork
+              </h1>
+              <p className='md:mt-[2%] mt-[4%] md:text-[14px] text-[16px] '>No more long queries , confusing forms , or endless frustration</p>
+              <div className='mt-[4%]'>
+                  <button className='bg-[#45BF6F] text-[14px] text-white px-2 py-2 hover:cursor-pointer rounded-md'>Get Started</button>
+                  <button className='font-bold px-4'>How It Works</button>
+              </div>
+              <div className='mx-auto  mt-[6%] text-center flex lg:flex-row flex-col justify-center'>
+                <Image
+                src={jobsAvailablebtn}
+                alt='Jobs Available btn'
+                style={{width:'140px',height:'44px'}}
+                className='mt-[8%] left-[18%] relative lg:flex hidden'
+                />
+                <Image
+                src={dottedLineImg}
+                alt='dooted-line-image'
+                className=' hidden absolute lg:flex mt-[2.5%]'
+                />
+                <Image 
+                src={heroSectionUser} 
+                alt="User Image"
+                className='relative md:h-[410px] md:w-[410px] md:-right-[4%]'
+                />
+                <Image
+                src={rentAgreementImg}
+                alt='Rent Aggrement Image'
+                style={{height:'300px',width:'300px'}}
+                className='lg:mt-[3%] lg:right-[9.5%] md:ml-[9%] relative '
+                />
+              </div>
+        </div>
+        <div className='lg:w-[85%] w-[90%] mt-[5%] mx-auto flex gap-x-[10px]'>
+           <button className='flex bg-[#EFEFEF] text-[12px] text-[#004016] p-2 gap-x-[10px] rounded-xl items-center'><Image style={{height:'24px',width:'24px'}} src={online} alt='online-img'/>Online Application</button>
+           <button className='flex bg-[#EFEFEF] text-[12px] text-[#004016] p-2 gap-x-[10px] rounded-xl items-center'><Image style={{height:'24px',width:'24px'}} src={call} alt='online-img'/>24/7 Support</button>
+           <button className='flex bg-[#EFEFEF] text-[12px] text-[#004016] p-2 gap-x-[10px] rounded-xl items-center'><Image style={{height:'24px',width:'24px'}} src={graph} alt='online-img'/>Expedicted Processing</button>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
