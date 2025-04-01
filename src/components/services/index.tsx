67190c257c91c0e65561da1f2d626f73c680ec2f
suryanshpandey61 <suryansh.knipss061@gmail.com>
2025-04-01 17:50:21 +0530

    services updated

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -6,7 +6,7 @@ import applicationReview from "../../assests/ApplicationReviewImg.png";
 
 function Services() {
   return (
-    <div className="lg:w-[85%] w-[90%]  rounded-3xl mx-auto bg-[#182D36] md:mt-[8%] lg:mt-[4%]">
+    <div className="lg:w-[85%] w-[92%] mt-9  rounded-3xl mx-auto bg-[#182D36] md:mt-[8%] lg:mt-[4%]">
       <div className="lg:w-[85%] w-[95%] mx-auto">
         {/* heading div  */}
         <div className="text-center ">
@@ -15,7 +15,7 @@ function Services() {
           </h1>
         </div>
         {/* text div  */}
-        <div className="text-center text-white font-normal lg:mt-0 md:mt-[3%] text-[20px]">
+        <div className="md:text-center  text-start mt-4 text-white font-normal lg:mt-0 md:mt-[3%] text-[20px]">
           <p>
             Mybabu Offers a range of services tailored to your needs.Our experts
             help you with
@@ -24,9 +24,9 @@ function Services() {
        {/* 4  cards div  */}
         <div className="mx-auto pb-[5%] lg:w-[90%] w-[97%] lg:mt-[2%] mt-[9%]">
           {/* top 2 cards  */}
-          <div className="flex  gap-x-4">
+          <div className="flex md:flex-row  flex-col gap-y-4  gap-x-4">
             {/* Document assistant card  */}
-            <div className="bg-[#FDF2D7] p-3  rounded-xl w-[45%] ">
+            <div className="bg-[#FDF2D7] p-3  rounded-xl md:w-[45%] w-full ">
               <h1 className="text-[16px] font-medium">Document Assistant</h1>
               <p className="text-[14px]">
                 Guidance of filling out forms,ensuring all required documents
@@ -39,7 +39,7 @@ function Services() {
               />
             </div>
             {/* Process Consultation card  */}
-            <div className="bg-[#FDF2D7] p-3 rounded-xl w-[60%]">
+            <div className="bg-[#FDF2D7] p-3 rounded-xl md:w-[60%] w-full">
               <h1 className="text-[16px] font-medium">Process Consultation</h1>
               <p className="text-[14px]">
                 Step by step instructions on navigation government
@@ -48,14 +48,14 @@ function Services() {
               <Image
                 src={processConsultatinImg}
                 alt="process consultation image"
-                className="mx-auto relative top-5.5"
+                className="mx-auto relative md:top-5.5 top-3"
               />
             </div>
           </div>
           {/* bottom 2 Cards  */}
-          <div className="flex mt-[2%] gap-x-4">
+          <div className="flex md:flex-row flex-col md:mt-[2%] mt-4 gap-y-4 gap-x-4">
             {/* Application Review card  */}
-            <div className="bg-[#FDF2D7] p-3 rounded-xl w-[60%]">
+            <div className="bg-[#FDF2D7] p-3 rounded-xl md:w-[60%] w-full">
               <h1 className="text-[16px] font-medium">Application Review</h1>
               <p className="text-[14px]">
                 Expert verification and corrections of your paperwork to increase the success
@@ -64,11 +64,11 @@ function Services() {
               <Image
                 src={applicationReview}
                 alt="process consultation image"
-                className="relative lg:top-3 md:top-11"
+                className="relative lg:top-3 md:top-6 top-3"
               />
             </div>
             {/* Personalized Support  card  */}
-            <div className="bg-[#FDF2D7] p-3  rounded-xl w-[45%] ">
+            <div className="bg-[#FDF2D7] p-3  rounded-xl md:w-[45%] w-full ">
               <h1 className="text-[16px] font-medium">Personalized Support</h1>
               <p className="text-[14px]">
                  One-one support via chat or in-person (as per location availability)
@@ -77,7 +77,7 @@ function Services() {
               <Image
                 src={personalizedImg}
                 alt="document assistant image"
-                className="relative lg:w-[150px] md:top-3 md:w-[180px]  lg:left-[57%] rounded-xl lg:top-[11.5%]"
+                className="relative lg:w-[150px] md:top-3 md:w-[180px] top-3  lg:left-[57%] rounded-xl lg:top-[11.5%]"
               />
             </div>
            
